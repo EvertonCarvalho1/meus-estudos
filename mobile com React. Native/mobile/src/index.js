151cb8c14a87ffad922f5818e29e878339b8383b
Everton Carvalho <toncarvalhosk@gmail.com>
2021-07-15 23:56:14 -0300

    Update

--- mobile com React. Native/mobile/src/index.js
+++ mobile com React. Native/mobile/src/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
 
 export default function App () {
-    return <View/>
+    return (
+    <>
+    
+        <StatusBar barStyle='dark-content' backgroundColor='#7159c1'/>
+
+        <View style={styles.container}> 
+
+            <Text style={styles.title}>Hello World</Text>
+        
+        </View>
+
+    </>
+)
 }
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'#7159c1',
+        flex: 1,
+        justifyContent: 'center'
+    },
+
+    title: {
+        color: '#FFF',
+        fontSize: 32,
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        
+    }
+});
 
