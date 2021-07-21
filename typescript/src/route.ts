7abeded81ff613963db33d8e2e5b4decb4743ca9
Everton Carvalho <toncarvalhosk@gmail.com>
2021-07-20 23:47:55 -0300

    update

--- typescript/src/route.ts
+++ typescript/src/route.ts
@@ -1,7 +1,15 @@
 import { Request, Response} from 'express';
+import createUser from './services/CreateUser';
 
 
 export function helloWorld(request: Request, response: Response) {
+
+    const user = createUser({
+        name: 'Everton Carvalho',
+        email: 'toncarvalhosk@gmail.com',
+        password: '123456789'
+
+    });
 
     return response.json({message: 'Hello World'});
 
---
+++ typescript/src/services/CreateUser.ts
@@ -0,0 +1,22 @@
+/**
+ * Para criar: name, email, password
+ * 
+ */
+
+interface CreateUserData {
+    name?: string,
+    email: string,
+    password: string,
+}
+
+export default function createUser({name, email, password}: CreateUserData){
+
+    const user = {
+        name,
+        email,
+        password
+    };
+
+    return user;
+
+};
