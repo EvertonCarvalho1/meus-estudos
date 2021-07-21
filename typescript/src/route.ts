b5691cb2f42baa8f3ad6ac702e115209350f386b
Everton Carvalho <toncarvalhosk@gmail.com>
2021-07-21 00:11:41 -0300

    update

--- typescript/src/route.ts
+++ typescript/src/route.ts
@@ -2,14 +2,23 @@ import { Request, Response} from 'express';
 import createUser from './services/CreateUser';
 
 
+//
+
 export function helloWorld(request: Request, response: Response) {
 
     const user = createUser({
         name: 'Everton Carvalho',
         email: 'toncarvalhosk@gmail.com',
-        password: '123456789'
-
+        password: '123456789',
+        techs: [
+            'Node.js', 
+            'React.js', 
+            'React Native',
+            {title: 'Javascript', experience: 100}
+        ],
     });
+
+    console.log();
 
     return response.json({message: 'Hello World'});
 
--- typescript/src/services/CreateUser.ts
+++ typescript/src/services/CreateUser.ts
@@ -3,18 +3,26 @@
  * 
  */
 
+interface TechObject {
+    title: string,
+    experience: number, 
+}
+
 interface CreateUserData {
     name?: string,
     email: string,
     password: string,
-}
+    techs: Array<string | TechObject>
+       
+};
 
-export default function createUser({name, email, password}: CreateUserData){
+export default function createUser({name, email, password, techs}: CreateUserData){
 
     const user = {
         name,
         email,
-        password
+        password,
+        techs
     };
 
     return user;
