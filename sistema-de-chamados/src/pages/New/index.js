5366c67ad3d708a7e33e14d98c13e8102a27eba7
Everton Carvalho <toncarvalhosk@gmail.com>
2021-09-03 06:59:21 -0300

    update

--- sistema-de-chamados/src/pages/New/index.js
+++ sistema-de-chamados/src/pages/New/index.js
@@ -4,7 +4,8 @@ import firebase from '../../services/firebaseConnection';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import {AuthContext} from '../../contexts/auth'
+import {AuthContext} from '../../contexts/auth';
+import { toast } from 'react-toastify';
 
 import {FiPlusCircle} from 'react-icons/fi'
 import './new.css';
@@ -28,7 +29,30 @@ export default function New (){
             await firebase.firestore().collection('customers')
             .get()
             .then((snapshot) =>{
-                
+
+                let lista = [];
+
+                snapshot.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        nomeFantasia: doc.data().nomeFantasia
+                    })
+                })
+
+                if(lista.length === 0){
+                    console.log('Nenhuma empresa encontrada');
+                    setCustomers([
+                        {
+                            id: '1', 
+                            nomeFantasia: 'Freela'
+                        }
+                    ])
+                    setLoadCustomers(false);
+                    return
+                }
+
+                setCustomers(lista);
+                setLoadCustomers(false);
             })
             .catch((error) => {
                 console.log('Deu erro' , error)
@@ -42,13 +66,36 @@ export default function New (){
             })
         }
 
+        loadCustomers();
 
-        loadCustomers()
     }, [])
 
-    function handleRegister (e) {
+    async function handleRegister (e) {
+
         e.preventDefault()
-        alert('teste')
+        
+        await firebase.firestore().collection('chamados')
+        .add({
+
+            created: new Date(),
+            cliente: customers[customerSelected].nomeFantasia,
+            clienteId: customers[customerSelected].id,
+            assunto: assunto,
+            status: status,
+            complemento: complemento,
+            userId: user.uid
+        })
+        .then(() => {
+            toast.success('Chamado criado com sucesso');
+            setComplemento('');
+            setCustomerSelected(0);
+        })
+        .catch((error) => {
+
+            toast.error('Ops, erro ao registrar, tente mais tarde.')
+            console.log(error);
+
+        })
     }
 
     //Chamada quando troca de assunto
@@ -61,6 +108,16 @@ export default function New (){
     function handleOptionChange (e) {
         setStatus(e.target.value);
       
+    }
+
+
+    //Chamada quando o cliente é trocado
+    function handleChangeCustomers (e){
+        console.log('index ',e.target.value);
+        console.log('cliente selecionado', customers[e.target.value]);
+
+
+        setCustomerSelected(e.target.value)
     }
 
     return(
@@ -78,11 +135,33 @@ export default function New (){
                     onSubmit={handleRegister}
                     >
                         <label>Cliente:</label>
-                        <select >
-                            <option key={1} value={1}>
-                                Everton Carvalho
-                            </option>
-                        </select>
+
+                        {loadCustomers ? (
+                            <input 
+                            type='text' 
+                            disabled={true} 
+                            value='Carregando clientes...'
+                            />
+                        ) : (
+                            <select 
+                            value={customerSelected} 
+                            onChange={handleChangeCustomers}
+                            >
+                               {customers.map((item, index) => {
+                                   return(
+                                    <option 
+                                    key={item.id}
+                                    value={index}
+                                    >
+                                        {item.nomeFantasia}
+    
+                                    </option>
+                                   )
+                               })}
+                            </select>
+                        )}
+
+                      
                         <label>Assunto:</label>
                         <select value={assunto} onChange={handleChengeSelect}>
                             <option value='Suporte'>Suporte</option>
