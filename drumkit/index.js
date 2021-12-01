22ae258de95f9a8a73f70dffbd0953c932407b1d
EvertonCarvalho1 <toncarvalhosk@gmail.com>
2021-12-01 13:56:34 -0300

    add codes

--- drumkit/index.js
+++ drumkit/index.js
@@ -1,3 +1,34 @@
 'use strict'; 
 
-const renderDiv  = 
+const songs = {
+    'A' : 'boom.wav',
+    'S' : 'clap.wav',
+    'D' : 'hihat.wav',
+    'F' : 'kick.wav',
+    'G' : 'openhat.wav',
+    'H' : 'ride.wav',
+    'J' : 'snare.wav',
+    'K' : 'tink.wav',
+    'L' : 'tom.wav',
+};
+
+const renderDiv  = (texto) => {
+    const div = document.createElement('div');
+    div.classList.add('key');
+    div.textContent = texto;
+    div.id = texto;
+    document.getElementById('container').appendChild(div);
+};
+
+const show = (songs) => {
+
+ const keys = Object.keys(songs);
+
+ keys.forEach(renderDiv)
+
+} 
+
+show (songs);
+
+
+
