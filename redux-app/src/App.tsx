f572ce3e710612da4b31c5669edaa03078b143d2
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-03-23 14:54:01 -0300

    development

--- redux-app/src/App.tsx
+++ redux-app/src/App.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { setName, setAge, setJob } from './redux/reducers/userReducer';
 import { useAppSelector } from './redux/hooks/useAppSelector';
+
 
 const App: React.FC = () => {
 
+  const dispatch = useDispatch();
   const user = useAppSelector(state => state.user);
- 
-  return(
+
+  const handleNameInput = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setName(event.target.value));
+  }, []);
+  const handleAgeInput = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setAge(event.target.value));
+  }, []);
+  const handleJobInput = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setJob(event.target.value));
+  }, []);
+
+  return (
     <div>
       Meu nome é {user.name} e tenho {user.age} anos. Traballho como {user.job}. <br />
 
       <hr />
+      <h3>Digite o nome</h3>
+      <input
+        type="text"
+        onChange={handleNameInput}
+      />
+      <hr />
+      <h3>Digite a idade</h3>
+      <input
+        type="text"
+        onChange={handleAgeInput}
+      />
 
-      <input type="text" value={'...'}/>
+      <hr />
+      <h3>Digite o trabalho</h3>
+      <input
+        type="text"
+        onChange={handleJobInput}
+      />
 
       <hr />
 
