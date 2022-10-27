e24d93805105645b0ce15c669694f2479bdead34
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-10-27 14:36:58 -0300

    creating methods

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -4,9 +4,50 @@ const res = require('express/lib/response');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: 'deu boa hoje!'}); 
+app.get('/courses', (request, response) => {
+    return response.json([
+        "Curso 1",
+        "Curso 2", 
+        "Curso 3"
+    ]); 
 });
+
+app.post('/courses', (request, response) => {
+    return response.json([
+        "Curso 1",
+        "Curso 2", 
+        "Curso 3",
+        "Curso 4"
+    ]); 
+});
+
+app.put('/courses/:id', (request, response) => {
+    return response.json([
+        "Curso 6",
+        "Curso 2", 
+        "Curso 3",
+        "Curso 4"
+    ]); 
+});
+
+app.patch('/courses/:id', (request, response) => {
+    return response.json([
+        "Curso 6",
+        "Curso 7", 
+        "Curso 3",
+        "Curso 4"
+    ]); 
+});
+
+app.delete('/courses/:id', (request, response) => {
+    return response.json([
+        "Curso 6",
+        "Curso 7", 
+        "Curso 4"
+    ]); 
+});
+
+
 
 app.listen(3333);
  
